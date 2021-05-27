c093d7ac5fc7ea5da39c5951de5ac552a4ed2a06
Mic <mic2110@gmx.de>
2021-05-28 00:38:35 +0200

    Pimp my Server + Unittest

--- Software/GebaeudeManagement/Server/BasicServer.h
+++ Software/GebaeudeManagement/Server/BasicServer.h
@@ -1,27 +1,44 @@
 #pragma once
-
-#include <boost/bind.hpp>
 #include <boost/asio.hpp>
-#include <boost/smart_ptr.hpp>
-#include <boost/thread/thread.hpp>
-#include "Raum.h"
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <list>
+#include <ctime>
+#include <mutex>
+#include <condition_variable>
 
-typedef boost::shared_ptr<boost::asio::ip::tcp::socket> socket_ptr;
+#include "Raum.h"
+#include "StreamBuffer.h"
 
 class BasicServer
 {
 public:
-	void start(int port);
-	BasicServer();
+	void update();
+
+	BasicServer(int port);
 	~BasicServer();
 	const static int max_length=1024;
 
 	bool read_config(char path[]);
+	bool end_flag = 0;
+
+#ifdef UNITTEST
+	void testrequest(char* request, char* answer);
+#endif
 
 private:
-	void session(socket_ptr sock);
-	virtual void processRequest(char request[], char answer[], socket_ptr sock);
-	virtual void send_log_file(const char log_file_path[], socket_ptr sock);
+	boost::asio::io_service io_service;
+	boost::asio::ip::tcp::tcp::acceptor* a;
+	std::list<boost::asio::ip::tcp::socket*> sockets;
+	std::list<std::thread*> threads;
+	std::list<bool> thread_callbacks;
+
+	void session();
+	void clear_up_lists();
+	static void asio_callback(const boost::system::error_code& ec);
+	void processRequest(char* request, char* answer, boost::asio::ip::tcp::socket* sock);
+	bool send_log_file(const char log_file_path[], boost::asio::ip::tcp::socket* sock);
 
 	std::vector<Raum> vec_raum;
 };
---
+++ Software/GebaeudeManagement/Server/Unittests.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <random>
+#include "BasicServer.h"
+
+class Unittests
+{
+public:
+	static void request_response_test(BasicServer* server);
+};
+
