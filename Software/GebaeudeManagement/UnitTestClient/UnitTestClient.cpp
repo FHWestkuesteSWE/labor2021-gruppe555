f5101f9e20c11de674f5b9b4c0407a8d3ea65773
ichdenkenicht <dix@goerlichs.de>
2021-06-07 11:57:23 +0200

    unittestclient2

--- Software/GebaeudeManagement/UnitTestClient/UnitTestClient.cpp
+++ Software/GebaeudeManagement/UnitTestClient/UnitTestClient.cpp
@@ -11,7 +11,7 @@ namespace UnitTestClient
 	{
 	public:
 		
-		TEST_METHOD(testmessage)
+		TEST_METHOD(testmessageopcode)
 		{
 			message* msg = new message();
 
@@ -20,6 +20,18 @@ namespace UnitTestClient
 			msg->makemsg();
 
 			Assert::AreEqual(std::string("test"), msg->msg_as_s);
+
+		}
+
+		TEST_METHOD(testmessageraum)
+		{
+			message* msg = new message();
+
+			msg->setraum("000");
+
+			msg->makemsg();
+
+			Assert::AreEqual(std::string("000"), msg->msg_as_s);
 
 		}
 	};
